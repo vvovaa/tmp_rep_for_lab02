2abe843ea3f2bf41ccb4a79498aba9a0b4e49fb4
Vova <v.o.v.a@mail.ru>
2022-03-19 12:16:04 +0300

    removed 'using namespace std;'

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-using namespace std;
 
 int main() {
     char name[255];
-    сin >> name;
-    cout << "Hello world from " << name << endl;
+    std::сin >> name;
+    std::cout << "Hello world from " << name << std::endl;
     return 0; 
 }
