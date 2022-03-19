7769ade73b2856d0fd6999f0fcccad96a6a9a70a
Vova <v.o.v.a@mail.ru>
2022-03-19 12:21:17 +0300

    added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
 int main() {
+    // Create variable for user name
     char name[255];
+
+    // Get name from user
     std::сin >> name;
+
+    // Our pretty business-process
     std::cout << "Hello world from " << name << std::endl;
+
     return 0; 
 }
